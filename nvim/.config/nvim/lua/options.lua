086e8dd53d0e6383ce4f64191a3fed0a7ec17ead
Thomas Ormezzano <thormezz@gmail.com>
2021-11-16 17:22:41 +0000

    set window title in vim

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -4,21 +4,29 @@ local cmd = vim.api.nvim_command
 vim.g.mapleader = " "
 
 --TODO review as some of these date from a very long time ago
-opt.number = true
 opt.mouse = "a"
+
 opt.ignorecase = true
 opt.smartcase = true
+
 opt.clipboard = "unnamedplus"
 opt.inccommand = "nosplit"
+
 opt.guicursor = ""
 opt.termguicolors = true
+opt.background = "dark"
+
 opt.splitbelow = true
 opt.splitright = true
-opt.background = "dark"
+
 opt.shiftwidth = 4
 opt.tabstop = 4
 opt.expandtab = true
+
+opt.number = true
 opt.cursorline = true
+
+opt.title = true
 
 cmd("colorscheme gruvbox")
 cmd("filetype plugin indent on")
