639df976c871d7ef89596e404455e46b10f7cefb
Thomas Ormezzano <thormezz@gmail.com>
2023-11-07 11:20:30 +0200

    improve treesitter config

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -44,4 +44,14 @@ return {
 		},
 	},
 	{ "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		keys = {
+			{ "<leader>xx", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
+			{ "<leader>xX", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
+			{ "<leader>xL", "<cmd>TroubleToggle loclist<cr>", desc = "Location List (Trouble)" },
+			{ "<leader>xQ", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List (Trouble)" },
+		},
+	},
 }
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,28 +1,31 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter-textobjects",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+
+			configs.setup({
+				auto_install = true,
+				sync_install = false,
+
+				highlight = {
+					enable = true, -- false will disable the whole extension
+				},
+				autopairs = {
+					enable = true,
+				},
+				indent = { enable = true },
+
+				context_commentstring = {
+					enable = true,
+					enable_autocmd = false,
+				},
+			})
+		end,
 	},
-	config = function()
-		local configs = require("nvim-treesitter.configs")
-
-		configs.setup({
-			auto_install = true,
-			sync_install = false,
-
-			highlight = {
-				enable = true, -- false will disable the whole extension
-			},
-			autopairs = {
-				enable = true,
-			},
-			indent = { enable = true },
-
-			context_commentstring = {
-				enable = true,
-				enable_autocmd = false,
-			},
-		})
-	end,
+	{ "windwp/nvim-ts-autotag", opts = {} },
 }
