b286b420adea3ec1bd9efd06b1ff2e15675d1403
Thomas Ormezzano <thomas@MacBookPro.mynet>
2026-06-08 10:57:44 +0100

    clean up fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,8 @@
-~/.local/bin/mise activate fish | source
+set -g fish_greeting
+mise activate fish | source
 if status is-interactive
     # Commands to run in interactive sessions can go here
     fzf --fish | source
     set -g fish_key_bindings fish_vi_key_bindings
 end
+alias nv='nvim'
